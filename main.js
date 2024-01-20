2d0630753501d5574e494546cb25049a9d76abb7
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-20 18:35:55 +0700

    add bootstrap

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
 import render from "./src/utils/common";
 import Navigo from "navigo";
 const app = document.getElementById("app");
